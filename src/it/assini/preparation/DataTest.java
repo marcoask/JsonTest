f8b6681ab709ef8ce08424756d618e89b57050e4
massini <marco.assini@optimo.it>
2016-05-03 11:15:50 +0200

    wip

--- src/it/assini/preparation/DataTest.java
+++ src/it/assini/preparation/DataTest.java
@@ -3,6 +3,7 @@ package it.assini.preparation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class DataTest {
@@ -15,33 +16,49 @@ public class DataTest {
 		String date1InString = "01-05-2016 10:00:00";
 		Date date1 = sdf.parse(date1InString);
 		
-		String date2InString = "01-05-2016 11:00:00";
+		String date2InString = "01-05-2016 10:11:00";
 		Date date2 = sdf.parse(date2InString);
 		
 		System.out.println("data1: " + date1 +"\ndata2: " + date2); //Tue Aug 31 10:20:56 SGT 1982
 		
-		long diff = getDateDiff(date1,date2,TimeUnit.MINUTES);
+		long diffMinutes = getDateDiffinUnit(date1,date2,TimeUnit.MINUTES);
 		
-		System.out.println("\nDifferenza in minuti: " + diff);
+		System.out.println("\nDifferenza in minuti: " + diffMinutes);
+
 		
 		//Dati n intervalli disponibili, divido l'intervallo temporale in n
 		System.out.println(separator);
-		int maxInterrogation = 10;
-		System.out.println("Divido l'intervvallo in "+maxInterrogation);
-		long oneInterval= diff/(maxInterrogation-1);
-		System.out.println("one Interval= "+oneInterval);
-		System.out.println("1:  "+date1);
-		for (int i=2; i<=maxInterrogation-1; i++){
-			long sum = date1.getTime()+oneInterval;
+		int maxInterrogation = 9;
+		System.out.println("Divido l'intervallo in "+maxInterrogation);
+		long oneIntervalMinutes= (diffMinutes/(maxInterrogation-1));
+		long oneIntervalMillisec= oneIntervalMinutes*1000*60;
+		System.out.println("oneIntervalMillisec= "+oneIntervalMillisec +" (minutes: " +oneIntervalMinutes+")");
+
+		long sum = date1.getTime();
+		for (int i=1; i<=maxInterrogation; i++){
 			
-			System.out.println(i+":  "+sum);
+			Date dateLoop = new Date(sum);
+			System.out.println(i+":  "+dateLoop+"\t"+getTravelTimeAtSpecifiedDate("A","B",dateLoop));
+			sum = sum+oneIntervalMillisec;
 			
 		}
-		System.out.println(maxInterrogation+":  "+date2);
 		
 		System.out.println(separator);
 			
 		//Data la lunghezza di un intervallo (m minuti), ricavo x intervalli lunghi m
+		System.out.println(separator);
+		oneIntervalMinutes = 10;
+		oneIntervalMillisec= oneIntervalMinutes*1000*60;
+		System.out.println("oneIntervalMillisec= "+oneIntervalMillisec +" (minutes: " +oneIntervalMinutes+")");
+		sum = date1.getTime();
+		int j = 1;
+		while (getDateDiffinUnit(date2,new Date(sum),TimeUnit.MINUTES)<0){
+			Date dateLoop = new Date(sum);
+			System.out.println(j+":  "+dateLoop+"\t"+getTravelTimeAtSpecifiedDate("A","B",dateLoop));
+			sum = sum+oneIntervalMillisec;
+			j++;
+		}
+		//System.out.println(maxInterrogation+":  "+date2);
 		
 	}
 	
@@ -52,9 +69,20 @@ public class DataTest {
 	 * @param timeUnit the unit in which you want the diff
 	 * @return the diff value, in the provided unit
 	 */
-	public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
+	public static long getDateDiffinUnit(Date date1, Date date2, TimeUnit timeUnit) {
 	    long diffInMillies = date2.getTime() - date1.getTime();
 	    return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+	}
+	
+	public static int getTravelTimeAtSpecifiedDate(String start, String stop, Date date){
+		Random random = new Random();
+		
+		int j = 3;
+		int n = 10-j;
+		int k = random.nextInt(n)+j;//Valori compresi tra 3 e 10
+		
+		return k;
+		
 	}
 
 }
